5de8fb257a047dc89c2318e56f67806fbcc15144
shivakumaravel <shivakumaravell@gmail.com>
2016-08-21 08:05:00 +0530

    Create inher.java

---
+++ inher.java
@@ -0,0 +1,18 @@
+# package com.sri.example;
+
+import java.util.Scanner;
+
+public class inher {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		 System.out.println("enter the value"); 
+		 Scanner s = new Scanner (System.in);
+		 
+		 int a =s.nextInt();
+		 int b =s.nextInt();
+		 System.out.println("The valu is :"+(a+b));
+	}
+
+}
